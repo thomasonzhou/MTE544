8aa75e97e020bc6c0e1cae652f4e21dc25a361d5
Thomason Zhou <t95zhou@uwaterloo.ca>
2025-11-10 17:55:45 -0500

    Add pf for localization and use as localizer for decisions

--- decisions.py
+++ decisions.py
@@ -46,7 +46,9 @@ class decision_maker(Node):
         self.reachThreshold=0.1
 
         # TODO part 5: call the proper types
-        self.localizer=localization(...)
+        # either use particlesFilter or rawSensors
+        self.localizer=localization(particlesFilter)
+        # self.localizer=localization(rawSensors)
         
         if motion_type==POINT_PLANNER:
             self.controller=controller(klp=0.05, klv=0.0, kap=0.8, kav=0.0)      
--- localization.py
+++ localization.py
@@ -57,13 +57,25 @@ class localization(Node):
 
     def odom_and_pf_pose_callback(self, odom_msg: odom, pf_msg: odom):
         # TODO: You need to use the pf_msg to update the pose of the robot [x, y, theta, stamp]
-        self.pose=[ ... ]
+
+        pf_pose = [
+            pf_msg.pose.pose.position.x, 
+            pf_msg.pose.pose.position.y, 
+            euler_from_quaternion(pf_msg.pose.pose.orientation)]
+        self.pose= [*pf_pose, pf_msg.header.stamp]
         
         # TODO: You need to log the values from the odom and the particle filter based on the headers
         # TODO: odom values: x, y, theta, vx, yawrate
-        odom_values_list = [...]
+
+        odom_values_list = [
+            odom_msg.pose.pose.position.x,
+            odom_msg.pose.pose.position.y,
+            euler_from_quaternion(odom_msg.pose.pose.orientation),
+            odom_msg.twist.twist.linear.x,
+            odom_msg.twist.twist.angular.z
+        ]
         # TODO: pf values: x, y, theta
-        pf_values_list = [...]
+        pf_values_list = pf_pose
 
         stamp = Time.from_msg(odom_msg.header.stamp).nanoseconds
         # Put all the values in a list
